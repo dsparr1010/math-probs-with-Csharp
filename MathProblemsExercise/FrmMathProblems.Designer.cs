c3ca3cbad1b6aaa3cabf5142f7017da3d8071a57
Debra <dsparr1010@gmail.com>
2020-07-20 10:21:02 -0500

    initial commit

---
+++ MathProblemsExercise/FrmMathProblems.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MathProblemsExercise
+{
+    partial class FrmMathProblems
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFindVol1 = new System.Windows.Forms.Button();
+            this.BtnSellingPrice = new System.Windows.Forms.Button();
+            this.BtnEmpPay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnFindVol1
+            // 
+            this.BtnFindVol1.Location = new System.Drawing.Point(55, 49);
+            this.BtnFindVol1.Name = "BtnFindVol1";
+            this.BtnFindVol1.Size = new System.Drawing.Size(163, 42);
+            this.BtnFindVol1.TabIndex = 0;
+            this.BtnFindVol1.Text = "Find Volume";
+            this.BtnFindVol1.UseVisualStyleBackColor = true;
+            this.BtnFindVol1.Click += new System.EventHandler(this.BtnFindVol1_Click);
+            // 
+            // BtnSellingPrice
+            // 
+            this.BtnSellingPrice.Location = new System.Drawing.Point(55, 133);
+            this.BtnSellingPrice.Name = "BtnSellingPrice";
+            this.BtnSellingPrice.Size = new System.Drawing.Size(163, 43);
+            this.BtnSellingPrice.TabIndex = 1;
+            this.BtnSellingPrice.Text = "Determine Selling Price Markup";
+            this.BtnSellingPrice.UseVisualStyleBackColor = true;
+            this.BtnSellingPrice.Click += new System.EventHandler(this.BtnSellingPrice_Click);
+            // 
+            // BtnEmpPay
+            // 
+            this.BtnEmpPay.Location = new System.Drawing.Point(55, 223);
+            this.BtnEmpPay.Name = "BtnEmpPay";
+            this.BtnEmpPay.Size = new System.Drawing.Size(163, 43);
+            this.BtnEmpPay.TabIndex = 2;
+            this.BtnEmpPay.Text = "Find Employee\'s Pay";
+            this.BtnEmpPay.UseVisualStyleBackColor = true;
+            this.BtnEmpPay.Click += new System.EventHandler(this.BtnEmpPay_Click);
+            // 
+            // FrmMathProblems
+            // 
+            this.ClientSize = new System.Drawing.Size(747, 468);
+            this.Controls.Add(this.BtnEmpPay);
+            this.Controls.Add(this.BtnSellingPrice);
+            this.Controls.Add(this.BtnFindVol1);
+            this.Name = "FrmMathProblems";
+            this.Text = "Solving Math Problems with C#";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFindVol;
+        private System.Windows.Forms.Button BtnFindVol1;
+        private System.Windows.Forms.Button BtnSellingPrice;
+        private System.Windows.Forms.Button BtnEmpPay;
+    }
+}
+
---
+++ MathProblemsExercise/FrmMathProblems.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathProblemsExercise
+{
+    public partial class FrmMathProblems : Form
+    {
+
+        public FrmMathProblems()
+        {
+            InitializeComponent();
+        }
+
+        //FIND VOLUME:
+        //Solve for volume formula:
+        // V = w h l
+        int length;
+        int width;
+        int height;
+        double volume;
+
+        //FIND MARKUP
+        float sellingprice;
+        float cost;
+        double markup;
+
+        //FIND EMP PAY
+
+
+
+        private void BtnFindVol1_Click(object sender, EventArgs e)
+        {
+            length = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter the LENGTH:", "Find the Volume of a Rectangular Solid", "", 200, 200));
+            width = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter the WIDTH:", "Find the Volume of a Rectangular Solid", "", 200, 200));
+            height = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter the HEIGHT:", "Find the Volume of a Rectangular Solid", "", 200, 200));
+
+            volume = (double) width * height * length;
+
+            MessageBox.Show("The volume of this rectangular solid is : " + volume);
+        }
+
+        private void BtnSellingPrice_Click(object sender, EventArgs e)
+        {
+            sellingprice = Convert.ToSingle(Microsoft.VisualBasic.Interaction.InputBox("What is the selling price?", "Determine the percentage markup of a store item", "", 200, 200));
+            cost = Convert.ToSingle(Microsoft.VisualBasic.Interaction.InputBox("What is th actual cost?", "Determine the percentage markup of a store item", "", 200, 200));
+
+            markup = (double)((sellingprice - cost) / cost) * 100;
+
+            MessageBox.Show("Markup percentage for item : " + markup + "%");
+        }
+
+        private void BtnEmpPay_Click(object sender, EventArgs e)
+        {
+            string emp_name;
+            float pay_rate;
+            float hours_worked;
+            double net_pay;
+            float gross_pay;
+
+            emp_name = Microsoft.VisualBasic.Interaction.InputBox("What is the Employee's Name", "Find Net Pay for an Employee", "", 200, 200);
+            pay_rate = Convert.ToSingle(Microsoft.VisualBasic.Interaction.InputBox("What is employee's rate of pay hourly?", "Find Net Pay for an Employee", "", 200, 200));
+            hours_worked = Convert.ToSingle(Microsoft.VisualBasic.Interaction.InputBox("How many hours did " + emp_name + " work?", "Find Net Pay for an Employee", "", 200, 200));
+
+            double tax_rate = .2;
+            double amount_deducted;
+
+            gross_pay = pay_rate * hours_worked;
+            amount_deducted = gross_pay * tax_rate;
+            net_pay = gross_pay - amount_deducted;
+
+            MessageBox.Show(
+                "The Gross Pay for " + emp_name + " is : " + gross_pay.ToString("c") + "\n" + "The Net Pay is : " + net_pay.ToString("c")
+                );
+        }
+    }
+}
